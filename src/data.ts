3a1926b24fcee8efc68e2a5f387e40bef82691ce
Julien Sulpis <julien.sulpis@gmail.com>
2021-11-06 12:33:11 +0100

    feat: add json data (#1)

--- src/data.ts
+++ src/data.ts
@@ -1,114 +1,15 @@
+import jsonData from "./data.json";
+
 export type Movie = {
   title: string;
-  imageUrl: string;
-  score: number;
+  type: "TVSeries" | "Movie";
   date: string;
+  directors: string;
+  score: string;
+  imageUrl: string;
+  description: string;
+  duration: string;
+  genres: string[];
 };
 
-export const data: Movie[] = [
-  {
-    title: "Queens Gambit",
-    score: Math.random(),
-    date: "2022-01-12",
-    imageUrl:
-      "https://www.themoviedb.org/t/p/w1280/jPHoAsRKtGKmjB28QgBWEtLJHRi.jpg",
-  },
-  {
-    title: "Squid Games",
-    score: Math.random(),
-    date: "2022-01-12",
-    imageUrl:
-      "https://www.themoviedb.org/t/p/w1280/jPHoAsRKtGKmjB28QgBWEtLJHRi.jpg",
-  },
-  {
-    title: "Peaky Blinders",
-    score: Math.random(),
-    date: "2022-01-12",
-    imageUrl:
-      "https://www.themoviedb.org/t/p/w1280/jPHoAsRKtGKmjB28QgBWEtLJHRi.jpg",
-  },
-  {
-    title: "Scherlock",
-    score: Math.random(),
-    date: "2022-01-12",
-    imageUrl:
-      "https://www.themoviedb.org/t/p/w1280/jPHoAsRKtGKmjB28QgBWEtLJHRi.jpg",
-  },
-  {
-    title: "Dunkerk",
-    score: Math.random(),
-    date: "2022-01-12",
-    imageUrl:
-      "https://www.themoviedb.org/t/p/w1280/jPHoAsRKtGKmjB28QgBWEtLJHRi.jpg",
-  },
-  {
-    title: "James Bond: No Time to die",
-    score: Math.random(),
-    date: "2022-01-12",
-    imageUrl:
-      "https://www.themoviedb.org/t/p/w1280/jPHoAsRKtGKmjB28QgBWEtLJHRi.jpg",
-  },
-  {
-    title: "Army of the dead",
-    score: Math.random(),
-    date: "2022-01-12",
-    imageUrl:
-      "https://www.themoviedb.org/t/p/w1280/jPHoAsRKtGKmjB28QgBWEtLJHRi.jpg",
-  },
-  {
-    title: "Eternals",
-    score: Math.random(),
-    date: "2022-01-12",
-    imageUrl:
-      "https://www.themoviedb.org/t/p/w1280/jPHoAsRKtGKmjB28QgBWEtLJHRi.jpg",
-  },
-  {
-    title: "Red notice",
-    score: Math.random(),
-    date: "2022-01-12",
-    imageUrl:
-      "https://www.themoviedb.org/t/p/w1280/jPHoAsRKtGKmjB28QgBWEtLJHRi.jpg",
-  },
-  {
-    title: "Arrival",
-    score: Math.random(),
-    date: "2022-01-12",
-    imageUrl:
-      "https://www.themoviedb.org/t/p/w1280/jPHoAsRKtGKmjB28QgBWEtLJHRi.jpg",
-  },
-  {
-    title: "Spiderman: no way home",
-    score: Math.random(),
-    date: "2022-01-12",
-    imageUrl:
-      "https://www.themoviedb.org/t/p/w1280/jPHoAsRKtGKmjB28QgBWEtLJHRi.jpg",
-  },
-  {
-    title: "Dune",
-    score: Math.random(),
-    date: "2022-01-12",
-    imageUrl:
-      "https://www.themoviedb.org/t/p/w1280/jPHoAsRKtGKmjB28QgBWEtLJHRi.jpg",
-  },
-  {
-    title: "Interstellar",
-    score: Math.random(),
-    date: "2022-01-12",
-    imageUrl:
-      "https://www.themoviedb.org/t/p/w1280/jPHoAsRKtGKmjB28QgBWEtLJHRi.jpg",
-  },
-  {
-    title: "Army of thieves",
-    score: Math.random(),
-    date: "2022-01-12",
-    imageUrl:
-      "https://www.themoviedb.org/t/p/w1280/jPHoAsRKtGKmjB28QgBWEtLJHRi.jpg",
-  },
-  {
-    title: "Avengers end game",
-    score: Math.random(),
-    date: "2022-01-12",
-    imageUrl:
-      "https://www.themoviedb.org/t/p/w1280/jPHoAsRKtGKmjB28QgBWEtLJHRi.jpg",
-  },
-];
+export const data = jsonData as Movie[];
