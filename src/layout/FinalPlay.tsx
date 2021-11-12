53a77d62d352ae7895aac817f37d98de795d14f3
Julien Sulpis <julien.sulpis@gmail.com>
2021-11-12 19:55:20 +0100

    feat: improve the animation when starting a movie

--- src/layout/FinalPlay.tsx
+++ src/layout/FinalPlay.tsx
@@ -1,6 +1,31 @@
-import { FC } from "react";
-import styled from "styled-components";
+import { FC, useEffect, useState } from "react";
+import styled, { keyframes } from "styled-components";
+import { CloseIcon } from "../icon/Close.icon";
 import { useMovies } from "../movie/movieContext";
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const title = keyframes`
+  0% {
+    opacity: 0;
+  }
+  25% {
+    opacity: 1;
+  }
+  75% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+`;
 
 const Container = styled.div`
   position: fixed;
@@ -8,20 +33,23 @@ const Container = styled.div`
   bottom: 0;
   right: 0;
   left: 0;
-  background-color: #424242;
+  background-color: black;
   color: white;
   display: none;
 
   &.displayed {
     display: flex;
     flex-direction: column;
+    justify-content: center;
+    animation: ${fadeIn} 1000ms ease forwards;
   }
 `;
 
 const Title = styled.h1`
   text-align: center;
-  margin-top: 10px;
-  margin-bottom: 20px;
+  font-size: 6rem;
+  opacity: 0;
+  animation: ${title} 2.5s ease 800ms forwards;
 `;
 
 const PlayerContainer = styled.div`
@@ -29,6 +57,11 @@ const PlayerContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  iframe {
+    width: 100%;
+    height: 85%;
+  }
 `;
 
 const CloseButton = styled.button`
@@ -51,15 +84,33 @@ export const FinalPlay: FC<{ display: boolean; onAction: () => void }> = ({
   onAction,
 }) => {
   const { cart } = useMovies();
+  const [startVideo, setStartVideo] = useState(false);
+  let timeout: any;
+
+  useEffect(() => {
+    if (display) {
+      timeout = setTimeout(() => {
+        setStartVideo(true);
+      }, 3000);
+    } else if (startVideo) {
+      setStartVideo(false); // reset to false when closing the player
+    }
+  }, [display, startVideo]);
+
+  function clickHandler() {
+    clearTimeout(timeout);
+    onAction();
+  }
 
   return (
     <Container className={display ? "displayed" : ""}>
-      <Title>Playing {cart[0]?.title}</Title>
-      <PlayerContainer>
-        {display && (
+      <CloseButton type="button" onClick={clickHandler}>
+        <CloseIcon size={50} fill="white"></CloseIcon>
+      </CloseButton>
+
+      {startVideo ? (
+        <PlayerContainer>
           <iframe
-            width="1120"
-            height="630"
             // src="https://www.youtube.com/embed/dQw4w9WgXcQ"
             src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
             title="YouTube video player"
@@ -67,12 +118,10 @@ export const FinalPlay: FC<{ display: boolean; onAction: () => void }> = ({
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           />
-        )}
-      </PlayerContainer>
-
-      <CloseButton type="button" onClick={onAction}>
-        X
-      </CloseButton>
+        </PlayerContainer>
+      ) : (
+        <Title>{cart[0]?.title}</Title>
+      )}
     </Container>
   );
 };
